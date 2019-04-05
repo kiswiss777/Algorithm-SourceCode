0b7425d5efb142f2704f4f9c42ac33142492a9fe
kiswiss777 <kiswiss77477@naver.com>
2019-04-05 18:31:05 +0900

    using namespace std;
    int solution(string arr) {
    	int answer = 0;
    	stack<int> sign; // ¿?¿? ¿?¿? ¿?¿?
    	char atArr;
    	for (int i = 0; i < arr.size(); i++)
    	{
    		atArr = arr[i];
    		if (atArr == '(')// ¿?¿?¿?¿?¿?¿? ¿?¿?¿? ¿?¿?
    			sign.push(1);
    		else
    		{
    			sign.pop(); // ¿?¿?¿? ¿?¿?¿? ¿?
    			if (arr[i - 1] == '(') //¿?¿?¿?¿?¿?¿? ¿?¿? ¿?¿?¿?¿?¿? ¿?¿?¿?
    				answer += sign.size();
    			else //¿?¿?¿?¿?¿? ¿?¿?¿?¿?¿?¿?¿? 1¿?¿?
    				answer++;
    		}
    	}
    	return answer;
    }

---
+++ Iron Rod/Iron Rod/IronRod.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string arr) {
+	int answer = 0;
+	stack<int> sign; // 부호 넣는 방법
+	char atArr;
+	for (int i = 0; i < arr.size(); i++)
+	{
+		atArr = arr[i];
+		if (atArr == '(')// 열리는경우는 무조건 푸쉬
+			sign.push(1);
+		else
+		{
+			sign.pop(); // 닫히면 무조건 팝
+			if (arr[i - 1] == '(') //레이저이니깐 앞의 사이즈만큼 더하기
+				answer += sign.size();
+			else //자기자신이 닫히는경우니깐 1개만
+				answer++;
+		}
+	}
+	return answer;
+}
